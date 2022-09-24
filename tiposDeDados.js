06f3fffa9d7a4861d4bf095a4fb53fafe0a2fc87
AbnerRodriguess <abnerrodriguesup@gmail.com>
2022-09-24 12:28:37 -0300

    tipos de dados

---
+++ tiposDeDados.js
@@ -0,0 +1,24 @@
+// **** TIPOS DE DADOS ****
+// Declare uma variável contendo uma string
+var nome = "Abner";
+// Declare uma variável contendo um número dentro de uma string
+var ano = "2022";
+
+// Declare uma variável com a sua idade
+var idade = 25;
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var sobrenome = "Rodrigues";
+// templatString
+var nomeFull = `${nome} ${sobrenome}`;
+console.log(nomeFull);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = " It's time";
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipo = typeof nome;
+
+console.log(verificarTipo);
+console.log(typeof nome);
