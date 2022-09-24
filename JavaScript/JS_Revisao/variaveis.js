d719bf9afa55f795a3ac2e44032c64099e0c8cc8
AbnerRodriguess <abnerrodriguesup@gmail.com>
2022-09-24 12:27:41 -0300

    Tipos de dados

--- JavaScript/JS_Revisao/variaveis.js
+++ JavaScript/JS_Revisao/variaveis.js
@@ -6,8 +6,7 @@ console.log(nome);
 // Declarar uma variável com a sua idade
 var idade = 25;
 console.log(idade);
-// Declarar uma variável com a sua comida
-// favorita e não atribuir valor
+// Declarar uma variável com a sua comida favorita e não atribuir valor
 var comida;
 // Atribuir valor a sua comida favorita
 comida = "Lanche";
