64f5c305083549d7d7b7c75eff7ea54f6e42b9d8
AbnerRodriguess <abnerrodriguesup@gmail.com>
2022-09-24 12:12:15 -0300

    Variaveis

--- JavaScript/JS_Revisao/script.js
+++ JavaScript/JS_Revisao/script.js
@@ -1,11 +1,16 @@
 // **** VARIÁVEIS ****
 // Declarar uma variável com o seu nome
+var nome = "Abner";
+console.log(nome);
 
 // Declarar uma variável com a sua idade
-
+var idade = 25;
+console.log(idade);
 // Declarar uma variável com a sua comida
 // favorita e não atribuir valor
-
+var comida;
 // Atribuir valor a sua comida favorita
-
+comida = "Lanche";
+console.log(comida);
 // Declarar 5 variáveis diferentes sem valores
+var endereco, cep, numero, telefone, sobrenome;
