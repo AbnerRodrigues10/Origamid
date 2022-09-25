045ef1fc0d3803619aa93335acd960495ae2f375
AbnerRodriguess <abnerrodriguesup@gmail.com>
2022-09-25 19:20:55 -0300

    condicionais

---
+++ JavaScript/JS_Revisao/05-boolenCondicionais1.js
@@ -0,0 +1,14 @@
+var possuiGraduacao = true;
+
+if (possuiGraduacao) {
+  console.log("Possui graduação");
+} else {
+  console.log("Não possui graduação");
+}
+// retorna Possui Graduação e não executa o else
+
+if (!possuiGraduacao) {
+  console.log("Não possui graduação");
+} else {
+  console.log("Possui graduação");
+}
