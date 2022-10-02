782bc806cd7394f10f003f2d31a8385f1c29f102
AbnerRodriguess <abnerrodriguesup@gmail.com>
2022-10-02 12:55:12 -0300

    Continuacao

---
+++ JavaScript/02-Projeto_AnimaisFantasticos/script.js
@@ -0,0 +1,13 @@
+// Retorne no console todas as imagens do site
+const imagens = document.getElementsByTagName("img");
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+
+const imagensNome = document.querySelectorAll("imagem");
+console.log(imagensNome);
+
+// Selecione todos os links internos (onde o href começa com #)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+// Selecione o último p do site
