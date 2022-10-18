ef841bf12837c9637c8cc823701a6f767d61c365
AbnerRodriguess <abnerrodriguesup@gmail.com>
2022-10-18 19:15:09 -0300

    Eventos part1

---
+++ JavaScript/02-Projeto_AnimaisFantasticos/05-Eventos/script.js
@@ -0,0 +1,49 @@
+const img = document.querySelector("img");
+
+function callback() {
+  console.log("clicou");
+}
+
+// img.addEventListener("click", callback);
+
+const animaisLista = document.querySelector(".animais-lista");
+
+function callbackLista(event) {
+  console.log(event.currentTarget);
+  console.log(event.target);
+}
+
+// animaisLista.addEventListener("click", callbackLista);
+
+// console.log(animaisLista);
+
+const linkExterno = document.querySelector('a[href^="http"]');
+
+function handleLinkExterno(event) {
+  event.preventDefault();
+  // console.log(event);
+  // console.log(this); é a mesma coisa de console.log(event.currentTarget);
+}
+
+// linkExterno.addEventListener("click", handleLinkExterno);
+
+const h1 = document.querySelector("h1");
+
+function handlerEvent(event) {
+  console.log(event.type, event);
+}
+
+// h1.addEventListener("click", handlerEvent);
+// h1.addEventListener("mouseenter", handlerEvent);
+// h1.addEventListener("mousemove", handlerEvent);
+// window.addEventListener("scroll", handlerEvent);
+// window.addEventListener("resize", handlerEvent);
+
+function handleKeyboard(event) {
+  if (event.key === "f") {
+    document.body.classList.toggle("fullscreen");
+  }
+  // console.log(event.key);
+}
+
+window.addEventListener("keydown", handleKeyboard);
