5322beaac3033ad6c99e449ac80cad11574bc9df
AbnerRodriguess <abnerrodriguesup@gmail.com>
2022-10-24 18:34:54 -0300

    Aumentar letra, mexer em todos elementos juntos

--- JavaScript/02-Projeto_AnimaisFantasticos/05-Eventos/scriptAula.js
+++ JavaScript/02-Projeto_AnimaisFantasticos/05-Eventos/scriptAula.js
@@ -1,7 +1,50 @@
 // Quando o usuário clicar nos links internos do site, adicione a classe ativo ao item clicado e remova dos demais itens caso eles possuam a mesma. Previna o comportamento padrão desses links
-// const links
+
+const linksInterno = document.querySelectorAll('a[href^="#"]');
+
+function handleLink(event) {
+  event.preventDefault();
+  linksInterno.forEach((link) => {
+    link.classList.remove("ativo");
+  });
+  event.currentTarget.classList.add("ativo");
+}
+
+linksInterno.forEach((link) => {
+  link.addEventListener("click", handleLink);
+});
+
 // Selecione todos os elementos do site começando a partir do body, ao clique mostre exatamente quais elementos estão sendo clicados
+
+const todosElementos = document.querySelectorAll("body *");
+
+function handleElemento(event) {
+  console.log(event.currentTarget);
+}
+
+todosElementos.forEach((elemento) => {
+  elemento.addEventListener("click", handleElemento);
+});
 
 // Utilizando o código anterior, ao invés de mostrar no console, remova o elemento que está sendo clicado, o método remove() remove um elemento
 
+// const todosElementoss = document.querySelectorAll("body *");
+
+// function handleElementos(event) {
+//   event.currentTarget.remove();
+// }
+
+// todosElementoss.forEach((elemento) => {
+//   elemento.addEventListener("click", handleElementos);
+// });
+
 // Se o usuário clicar na tecla (t), aumente todo o texto do site.
+
+function handleClickT(event) {
+  console.log(event.key);
+  if (event.key === "t") {
+    document.documentElement.classList.toggle("textomaior");
+  }
+}
+
+window.addEventListener("keydown", handleClickT);
